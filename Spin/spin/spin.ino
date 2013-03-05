6fde00391586785c4d226ca09f0be9ebff441bf0
Eric Tytell <eric.tytell@tufts.edu>
2013-03-05 17:30:30 -0500

    Changed spin delay; added encoder
    Instead of trying to run at a constant rate and count up to a step
    pulse, it just delays until the right time.  Seems to work better.
    Reading the encoder takes a while, which limits the maximum speed

--- Spin/spin/spin.ino
+++ Spin/spin/spin.ino
@@ -1,53 +1,60 @@
-int motorStepPin = 2;
-int motorDirPin = 4;
-int motorEnablePin = 3;
+#include <Encoder.h>
+
+int motorStepPin = 4;
+int motorDirPin = 6;
+int motorEnablePin = 5;
 int stepsperrev = 6400;
-unsigned long outputrate = 100000;
-unsigned long sampledelay;
+long pulsedelay, pulsedelay2;
+// length of time to output a pulse and read the encoder in us
+// determined by oscilloscope.  And there's a bunch of jitter,
+// so this is an eyeball average
+int readdur = 88;
 
 float spinrate = 0;
-float samplesperstep = 0;
-unsigned int cursamp = 0;
+
+// Encoder pins.  Need to have interrupts on the pins, and on
+// this Arduino, that's only 2 and 3
+Encoder encoder(2, 3);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   pinMode(motorStepPin, OUTPUT);
   pinMode(motorDirPin, OUTPUT);
   pinMode(motorEnablePin, OUTPUT);
   digitalWrite(motorEnablePin, HIGH);
-  
-  sampledelay = long(1000000/outputrate);
 }
 
 void loop()
 {
-  float rate1;
+  long pos1, pos2;
   
   if (Serial.available()) {
     spinrate = Serial.parseFloat();
-    samplesperstep = (outputrate / (abs(spinrate) * stepsperrev)) + 0.5;
-    Serial.print("Output delay (us): ");
-    Serial.println(sampledelay);
-    Serial.print("samplesperstep: ");
-    Serial.println(samplesperstep);
-    if (samplesperstep <= 1) {
+    pulsedelay = 1000000 / (abs(spinrate) * stepsperrev) - readdur;
+    Serial.print("Pulse delay (us): ");
+    Serial.println(pulsedelay);
+    if (pulsedelay <= 0) {
       Serial.println("Error: movement too fast");
-      samplesperstep = 0;
+      spinrate = 0;
+    }
+    
+    // set the direction
+    if (spinrate > 0) {
+      digitalWrite(motorDirPin, HIGH);
+    }
+    else {
+      digitalWrite(motorDirPin, LOW);
     }
   }
   
-  if (spinrate > 0) {
-    cursamp++;
-    if (cursamp >= samplesperstep) {
-      cursamp = 0;
-      digitalWrite(motorStepPin, HIGH);
-    }
-    else {
-      digitalWrite(motorStepPin, LOW);
-    }    
-    delayMicroseconds(sampledelay);
+  if (spinrate != 0) {
+    pos1 = encoder.read();
+    digitalWrite(motorStepPin, HIGH);
+    digitalWrite(motorStepPin, LOW);
+    Serial.write(pos1);
+    delayMicroseconds(pulsedelay);
   }
 }
       
