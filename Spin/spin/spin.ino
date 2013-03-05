3d5ba9b1352c7aec8a5470c743d0ca8e0567213f
Eric Tytell <eric.tytell@tufts.edu>
2013-03-05 16:11:52 -0500

    Working basic structure, but spins slowly

--- Spin/spin/spin.ino
+++ Spin/spin/spin.ino
@@ -1,13 +1,13 @@
 int motorStepPin = 2;
 int motorDirPin = 4;
 int motorEnablePin = 3;
-int stepsperrev = 2048;
-int outputrate = 10000;
-int sampledelay;
+int stepsperrev = 6400;
+unsigned long outputrate = 100000;
+unsigned long sampledelay;
 
 float spinrate = 0;
-int samplesperstep = 0;
-int cursamp = 0;
+float samplesperstep = 0;
+unsigned int cursamp = 0;
 
 void setup()
 {
@@ -15,8 +15,10 @@ void setup()
   
   pinMode(motorStepPin, OUTPUT);
   pinMode(motorDirPin, OUTPUT);
+  pinMode(motorEnablePin, OUTPUT);
+  digitalWrite(motorEnablePin, HIGH);
   
-  sampledelay = int(1/outputrate * 1e6);
+  sampledelay = long(1000000/outputrate);
 }
 
 void loop()
@@ -24,41 +26,27 @@ void loop()
   float rate1;
   
   if (Serial.available()) {
-    rate1 = Serial.parseFloat();
-    if (rate1 != 0) {
-      spinrate = rate1;
-    }
-    samplesperstep = int(outputrate / (abs(spinrate) * stepsperrev) + 0.5);
+    spinrate = Serial.parseFloat();
+    samplesperstep = (outputrate / (abs(spinrate) * stepsperrev)) + 0.5;
+    Serial.print("Output delay (us): ");
+    Serial.println(sampledelay);
+    Serial.print("samplesperstep: ");
+    Serial.println(samplesperstep);
     if (samplesperstep <= 1) {
       Serial.println("Error: movement too fast");
       samplesperstep = 0;
     }
-    else {
-      Serial.print("Output delay (us): ");
-      Serial.println(sampledelay);
-      Serial.print("samplesperstep: ");
-      Serial.println(samplesperstep);
-    }
   }
   
   if (spinrate > 0) {
-  
     cursamp++;
-    if (cursamp == samplesperstep) {
+    if (cursamp >= samplesperstep) {
       cursamp = 0;
       digitalWrite(motorStepPin, HIGH);
     }
     else {
       digitalWrite(motorStepPin, LOW);
-    }
-    
-    if (spinrate > 0) {
-      digitalWrite(motorDirPin, HIGH);
-    }
-    else {
-      digitalWrite(motorDirPin, LOW);
-    }
-    
+    }    
     delayMicroseconds(sampledelay);
   }
 }
